827edbfd0d51619cd02582e24f6be2ca467b5ca4
Marshal-bd <arfurm198@gmail.com>
2025-11-05 16:12:42 +0700

    test2

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
 import usersRoutes from "./routes/users.routes.js";
 
-dotenv.config();
+// dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
