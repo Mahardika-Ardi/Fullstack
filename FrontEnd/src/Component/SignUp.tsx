fbf63383b29282f5a72b0c2dae0857c86c87c3a7
Marshal-bd <arfurm198@gmail.com>
2025-11-05 18:22:52 +0700

    Test7

--- FrontEnd/src/Component/SignUp.tsx
+++ FrontEnd/src/Component/SignUp.tsx
@@ -16,7 +16,7 @@ export default function SignUp() {
     setError("");
 
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/Users/SignUp`, {
+      const res = await fetch(`https://backed.up.railway.app/Users/SignUp`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
